70c73b154992180db9bdc666349646ff54879664
Ruben Talavera <rtalaveragarcia@ucm.es>
2024-12-11 13:04:01 +0100

    update map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,19 +5,9 @@ var addressPoints = [
     -3.7035825
   ],
   [
-    "Santiago de Chile, Chile",
-    -33.4377756,
-    -70.6504502
-  ],
-  [
     "Liverpool, United Kingdom",
     53.4071991,
     -2.99168
-  ],
-  [
-    "Lisbon, Portugal",
-    38.7077507,
-    -9.1365919
   ],
   [
     "Porto, Portugal",
@@ -25,9 +15,39 @@ var addressPoints = [
     -8.6107884
   ],
   [
+    "Lisbon, Portugal",
+    38.7077507,
+    -9.1365919
+  ],
+  [
+    "Santiago de Chile, Chile",
+    -33.4377756,
+    -70.6504502
+  ],
+  [
+    "San Francisco, California",
+    37.7792588,
+    -122.4193286
+  ],
+  [
+    "Berkeley CA, USA",
+    37.8708393,
+    -122.272863
+  ],
+  [
     "Dublin, Ireland",
-    53.3498006,
-    -6.2602964
+    53.3493795,
+    -6.2605593
+  ],
+  [
+    "London, UK",
+    51.4893335,
+    -0.14405508452768728
+  ],
+  [
+    "Los Angeles, CA",
+    34.0536909,
+    -118.242766
   ],
   [
     "Granada, Spain",
@@ -38,5 +58,20 @@ var addressPoints = [
     "Zaragoza, Spain",
     41.6521342,
     -0.8809428
+  ],
+  [
+    "C\u00e1ceres, Spain",
+    39.4020024,
+    -6.454580525853476
+  ],
+  [
+    "Coimbra, Portugal",
+    40.2111931,
+    -8.4294632
+  ],
+  [
+    "Santa Fe de la Veracruz, Argentina",
+    -31.6417633,
+    -60.7049979
   ]
 ];
