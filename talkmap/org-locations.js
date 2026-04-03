faccbd3099912bb6bbf802ea82722c38854ddfcd
Ruben Talavera-Garcia <rtalaveragarcia@ucm.es>
2026-04-03 10:21:49 +0200

    update site

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,67 +1,22 @@
 var addressPoints = [
   [
-    "Madrid, Spain",
-    40.4167047,
-    -3.7035825
-  ],
-  [
-    "Liverpool, United Kingdom",
-    53.4071991,
-    -2.99168
-  ],
-  [
-    "Porto, Portugal",
-    41.1494512,
-    -8.6107884
-  ],
-  [
-    "Lisbon, Portugal",
-    38.7077507,
-    -9.1365919
-  ],
-  [
-    "Santiago de Chile, Chile",
-    -33.4377756,
-    -70.6504502
-  ],
-  [
-    "Dublin, Ireland",
-    53.3493795,
-    -6.2605593
-  ],
-  [
-    "London, UK",
+    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
     51.4893335,
     -0.14405508452768728
   ],
   [
-    "Granada, Spain",
-    37.1734995,
-    -3.5995337
+    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
+    37.8708393,
+    -122.272863
   ],
   [
-    "Zaragoza, Spain",
-    41.6521342,
-    -0.8809428
+    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
+    34.0536909,
+    -118.242766
   ],
   [
-    "C\u00e1ceres, Spain",
-    39.4745175,
-    -6.3716761
-  ],
-  [
-    "Coimbra, Portugal",
-    40.2111931,
-    -8.4294632
-  ],
-  [
-    "Santa Fe de la Veracruz, Argentina",
-    -31.6417633,
-    -60.7049979
-  ],
-  [
-    "Palma de Mallorca, Espa\u00f1a ",
-    39.5695818,
-    2.6500745
+    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
+    37.7792588,
+    -122.4193286
   ]
 ];
