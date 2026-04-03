2a598fe43c0bd27a699f9f142bab501cabdc97aa
Ruben Talavera-Garcia <rtalaveragarcia@ucm.es>
2026-04-03 15:20:04 +0200

    map talk

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,102 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
+    "Introducci\u00f3n a ArcGIS Pro<br />Universidad Complutense de Madrid; Madrid, Spain",
+    40.416782,
+    -3.703507
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Big Data en la planificaci\u00f3n del transporte urbano<br />Universidad Nacional del Litoral; Santa Fe de la Veracruz, Argentina",
+    19.2119255,
+    -96.2717223
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Introducci\u00f3n al software libre QGIS<br />Facultad de Geograf\u00eda e Historia, Universidad Complutense de Madrid; Madrid, Spain",
+    40.416782,
+    -3.703507
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "An\u00e1lisis r\u00e1ster con QGIS<br />Faculty of Geography and History, Complutense University of Madrid; Madrid, Spain",
+    40.416782,
+    -3.703507
+  ],
+  [
+    "Improving pedestrian accessibility to public space through space syntax analysis<br />Pont\u00edfica Universidad Cat\u00f3lica de Chile; Santiago de Chile, Chile",
+    -33.4376995,
+    -70.6510671
+  ],
+  [
+    "Evaluaci\u00f3n del efecto de la pandemia de COVID-19 en el uso del transporte p\u00fablico en la ciudad de Santa Fe de la Vera Cruz, Argentina<br />Universidad de Castilla la Mancha; C\u00e1ceres, Spain",
+    39.4745175,
+    -6.3716761
+  ],
+  [
+    "Who uses Madrid's parks and when?: estimates from mobile phone data usage<br />University of Coimbra; Coimbra, Portugal",
+    40.2111931,
+    -8.4294632
+  ],
+  [
+    "Introducci\u00f3n al software libre QGIS<br />Faculty of Geography and History, Complutense University of Madrid; Madrid, Spain",
+    40.416782,
+    -3.703507
+  ],
+  [
+    "Light rail and planning. Innovations in Andalusian metropolitan areas<br />Liverpool University; Liverpool, United Kingdom",
+    53.4071991,
+    -2.99168
+  ],
+  [
+    "Spatial configuration analysis to improve the accessibility to light-rail stations<br />Universidade Nova de Lisboa; Lisbon, Portugal",
+    38.7077507,
+    -9.1365919
+  ],
+  [
+    "Integraci\u00f3n QGIS \u2013 Inkscape para cartograf\u00eda<br />Faculty of Geography and History, Complutense University of Madrid; Madrid, Spain",
+    40.416782,
+    -3.703507
+  ],
+  [
+    "Parques como espacios de mezcla socioecon\u00f3mica: An\u00e1lisis de Madrid a partir datos de telefon\u00eda m\u00f3vil.<br />Universidad de Extremadura; C\u00e1ceres, Espa\u00f1a",
+    39.4745175,
+    -6.3716761
+  ],
+  [
+    "An\u00e1lisis de patrones de movilidad tur\u00edstica en Madrid mediante datos de telefon\u00eda m\u00f3vil.<br />Universidad de las Islas Baleares; Palma de Mallorca, Espa\u00f1a",
+    39.5695818,
+    2.6500745
+  ],
+  [
+    "Big Data geolocalizado<br />Universidad Nacional del Litoral; Santa Fe de la Veracruz, Argentina",
+    19.2119255,
+    -96.2717223
+  ],
+  [
+    "La contribuci\u00f3n de las Smart Cities a los ODS<br />Facultad de Ciencias Qu\u00edmicas, Universidad Complutense de Madrid; Madrid, Spain",
+    40.416782,
+    -3.703507
+  ],
+  [
+    "A framework to assess the innovation originated by the light rail in the Andalusian metropolitan areas<br />Madrid; Madrid, Spain",
+    40.416782,
+    -3.703507
+  ],
+  [
+    "Pedestrian accessibility to public space: the implication of the urban form in the city of Granada<br />Porto University; Porto, Portugal",
+    41.1502195,
+    -8.6103497
+  ],
+  [
+    "Pedestrian mobility: definition, evaluation and prospects<br />University College Dublin; Dublin, Ireland",
+    53.3493795,
+    -6.2605593
+  ],
+  [
+    "Walking distance to transit: making public transport more human and successful<br />Facultad de Arquitectura, Universidad de Granada; Granada, Spain",
+    37.1734995,
+    -3.5995337
+  ],
+  [
+    "Evaluaci\u00f3n del nivel de servicio peatonal en Madrid a partir de datos de sensores fijos<br />Universidad de Zaragoza; Zaragoza, Spain",
+    41.6521342,
+    -0.8809428
   ]
 ];
